4ae76edd66379a3242521bcd6629d2211097fc25
dingcaiying <caiying@it-consultis.com>
2018-04-03 14:38:40 +0800

    add pointlight

--- src/js/main.js
+++ src/js/main.js
@@ -13,8 +13,10 @@ const musicUrls = [
 ];
 
 let container, stats;
-let camera, scene, renderer, particles, particle, count = 0;
+let camera, scene, renderer, particles, particle;
+let ambientLight, particleLight; // particleLight: mesh contain pointLight, make light visible
 let mouseX = 0, mouseY = 0;
+let count = 0;
 
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
@@ -23,41 +25,56 @@ let raycaster, mouse;
 
 let sound;
 let analyser, freqByteData;
+let panner;
+let rad = 0;
 
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+const sceneHeight = 2000;
+const cameraRatio = window.innerWidth / window.innerHeight;
+const sceneWidth = cameraRatio * sceneHeight;
 
 initSound()
   .then(initScene)
   .then(bindEvent)
   .then(animate);
 
+// scene size: height: 2000, width: cameraRatio * height
 function initScene() {
   container = document.createElement('div');
   document.body.appendChild(container);
 
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 3000);
-  camera.position.z = 1000;
+  camera = new THREE.PerspectiveCamera(75, cameraRatio, 1, 3000);
+  camera.position.z = sceneHeight / (2 * Math.atan(75 / 2));
 
   scene = new THREE.Scene();
 
+  ambientLight = new THREE.AmbientLight(0x666666);
+  scene.add(ambientLight);
+
+  // var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  // directionalLight.position.set(1, 1, 1).normalize();
+  // scene.add(directionalLight);
+
+  var pointLight = new THREE.PointLight(0xffffff, 5, 800);
+  particleLight = new THREE.Mesh(new THREE.SphereBufferGeometry(8, 32, 32), new THREE.MeshBasicMaterial({ color: 0xffffff } ));
+  particleLight.position.set(0, 0, 0);
+  particleLight.add(pointLight);
+  scene.add(particleLight);
+
+
   particles = new Array();
 
-  const PI2 = Math.PI * 2;
-  const program = context => {
-    context.beginPath();
-    context.arc(0, 0, 10, 0, PI2, true);
-    context.fill();
-  };
-
   for (let i = 0; i < freqByteData.length; i++) {
-    const material = new THREE.MeshBasicMaterial({
+    const material = new THREE.MeshPhongMaterial({
       color: Math.random() * 0x808008 + 0x808080,
+      shininess: 50
     });
-    particle = particles[i] = new THREE.Mesh(new THREE.CircleGeometry(5, 64 ), material);
+    particle = particles[i] = new THREE.Mesh(new THREE.CircleGeometry(5, 64), material);
 
-    particle.position.x = Math.random() * 2000 - 1000;
-    particle.position.y = Math.random() * 2000 - 1000;
-    particle.position.z = Math.random() * 2000 - 1000;
+    particle.position.x = randomNumberInRange(-sceneWidth / 2 + 100, sceneWidth / 2 - 100);
+    particle.position.y = randomNumberInRange(-sceneHeight / 2 + 100, sceneHeight / 2 - 100);
+    particle.position.z = randomNumberInRange(-camera.position.z, camera.position.z); // camera.position.z > 0
     scene.add(particle);
   }
 
@@ -76,22 +93,37 @@ function initSound(url = '/assets/music/example.mp3') {
   return loadSound(url).then(buffer => {
     sound = new Sound(audioCtx, buffer);
 
+    panner = audioCtx.createPanner();
+
     analyser = audioCtx.createAnalyser();
     analyser.fftSize = 128;
+
+    sound.setOutput(panner);
+    panner.connect(analyser);
     analyser.connect(audioCtx.destination);
 
     freqByteData = new Uint8Array(analyser.frequencyBinCount);
 
-    sound.setOutput(analyser);
     sound.play();
   });
 }
 
 function render() {
   count++;
-  camera.position.x += (mouseX - camera.position.x) * 0.05;
-  camera.position.y += (- mouseY - camera.position.y) * 0.05;
-  camera.lookAt(scene.position);
+  // camera.position.x += (mouseX - camera.position.x) * 0.05;
+  // camera.position.y += (- mouseY - camera.position.y) * 0.05;
+  // camera.lookAt(scene.position);
+
+  rad += 1;
+  if (rad > 360) rad -= 360;
+  const nx = Math.sin(rad * Math.PI / 180);
+  const ny = Math.cos(rad * Math.PI / 180);
+  const nz = Math.cos(rad * Math.PI / 180);
+  particleLight.position.x = nx * 300;
+  particleLight.position.y = ny * 300;
+  particleLight.lookAt(scene.position);
+  panner.setPosition(nx, ny, nz);
+
   updateFFT();
 
   let scaler;
@@ -159,4 +191,8 @@ function bindEvent() {
 
   // intersection
   document.addEventListener('dblclick', onDocumentMouseDown, false);
+}
+
+function randomNumberInRange(min, max) {
+  return Math.random() * (max - min) + min;
 }
